8ada12e6f976f804959f5ae784c3bf834721c955
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-02-26 16:35:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ img="";
 Status="";
 objects=[];
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(380,380);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
     objectDetector=ml5.objectDetector('cocossd',modelloaded);
     document.getElementById("status").innerHTML="Status: Detecting Objects";
 }
@@ -11,15 +13,20 @@ function preload(){
     img=loadImage('dog_cat.jpg');
 }
 function draw(){
-    image(img,0,0,640,420);
+    image(video,0,0,380,380);
     if(Status!=""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotresults);
         for(i=0;i<objects.length;i++){
             document.getElementById("status").innerHTML="Status=Objects Detected";
-            fill("Red");
-            percent=floor(objects[i].cofidence*100);
+            document.getElementById("no_of_obj").innerHTML="Number of objects detected are : "+objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
             text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
             noFill();
-            stroke("blue");
+            stroke(r,g,b);
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
         }
     }
@@ -27,7 +34,6 @@ function draw(){
 function modelloaded(){
     console.log("Model Loaded");
     Status=true;
-    objectDetector.detect(img,gotresults);
 }
 function gotresults(error,results){
      if(error){
